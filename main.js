2ea485d72b61653de5c2025f31e027a3aac704e6
ABSylvain <arnaudbouriquesylvain@gmail.com>
2017-09-05 13:56:03 +0200

    commit

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var hostname = 'localhost';
 var port = 9000;
-
+import todo from './todo.ts';
 app.listen(9000, 'localhost', function() {
     console.log('Server ok, ' + hostname + ' : ' + port);
 })
+
+todo.add("blop");
